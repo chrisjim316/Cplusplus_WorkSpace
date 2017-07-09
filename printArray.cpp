8e2f973c5d52a896ad8364d6665afd02dde25e3d
Chris Jim <chrisjim316@gmail.com>
2017-07-10 00:00:39 +0800

    Create printArray.cpp

---
+++ printArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class printArray {
+
+  void print1DArray(const char arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout << arr[i];
+    }
+    cout << endl;
+  }
+  
+
+  void print2Darray(const char arr[][SIZE], int rows, int cols){
+    for(int r = 0; r < rows; r++){
+        print1DArray(arr[r], cols);
+    }
+  }
+  
+};
+
