b2eb1aab02d57402402651e5f236036e570feba2
Chris Jim <chrisjim316@gmail.com>
2017-07-10 00:06:04 +0800

    Update addValueToChar.cpp

--- addValueToChar.cpp
+++ addValueToChar.cpp
@@ -1,40 +1,41 @@
-// Requires: The size of array arr is size && size > 0 && size <= SIZE
-// Modifies: The array arr
-// Effects:  Adds val to each character in the array
-//           If the array is: how now brown cow
-//           and val = 5,
-//           then result is:  mt|%st|%gwt|s%ht|
-// Note:     Make sure the character remains in the ' ' MIN_PRINTABLE_ASCII
-//           to '~' MAX_PRINTABLE_ASCII range (inclusive)
-//           all printable characters are between this range
-//           If character < MIN_PRINTABLE_ASCII, then add ASCII_PRINTABLE_RANGE
-//           If character > MAX_PRINTABLE_ASCII, then subtract ASCII_PRINTABLE_RANGE
-//            to put back within range,  repeat as needed
-//           all printable characters are between this range
-//           ' ' is the 1st printable character (ASCII 32)
-//           '~' is the last printable character (ASCII 126)
-// Note:     Do not do the addition on char's. It could cause an overflow
+//random test num  
+const int SIZE = 60;
 
-void add(char arr[], int size, int val) {
+const int MAX_PRINTABLE_ASCII = '~';
+const int MIN_PRINTABLE_ASCII = ' ';
+const int ASCII_PRINTABLE_RANGE = MAX_PRINTABLE_ASCII - MIN_PRINTABLE_ASCII + 1;
 
-    if(size > 0){
-
-    int charInt = 0;
-
+class addValueToChar {
+    
+    // Requires: The size of array arr is size && size > 0 && size <= SIZE
+    // Modifies: The array arr
+    // Effects:  Adds val to each character in the array
+    //           If the array is: how now brown cow
+    //           and val = 5,
+    //           then result is:  mt|%st|%gwt|s%ht|
+    
+    void add1DArray(char arr[], int size, int val) {
+      if(size > 0){
+        int charInt = 0;
             for (int i = 0; i < size; i++) {
-
                 charInt = ((int)arr[i] + val);
-
                 while (charInt < MIN_PRINTABLE_ASCII) {
                     charInt += ASCII_PRINTABLE_RANGE;
                 }
-
                 while (charInt > MAX_PRINTABLE_ASCII) {
                     charInt -= ASCII_PRINTABLE_RANGE;
                 }
                     arr[i] = ((char)charInt);
             }
-
+       }
     }
-
-}
+    
+    void add2DArray(char arr[][SIZE], int rows, int cols, int val){
+       if(rows > 0 && cols > 0){
+           for(int r = 0; r < rows; r++){
+               add1DArray(arr[r], cols, val);
+           }
+       }
+    }
+    
+};
